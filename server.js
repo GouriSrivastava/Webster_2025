a227cfd0a6da44fb1ae8ffd35fc868344730ae94
Gouri <igouri@gmail.com>
2025-10-06 22:46:11 +0530

    server

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import passport from "passport";
+import connectDB from "/config/database.js";
+import "/config/passport.js";
+import authRoutes from "/routes/auth.js";
+dotenv.config();
+connectDB();
+const app=express();
+app.use(cors());
+app.use(express.json());
+app.use(passport.initialize());
+app.use("/api/auth",authRoutes);
+const PORT=process.env.PORT || 5000;
+app.listen(PORT,()=> console.log(`Server is running on http://localhost:${PORT}`));
+
+
