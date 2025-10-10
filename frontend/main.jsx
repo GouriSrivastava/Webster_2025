35c87afeba5b8fa9abec84aa4fe1b25894189f92
GouriSrivastava <igouri@gmail.com>
2025-10-10 12:46:13 +0530

    Update main.jsx

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <GoogleOAuthProvider clientId="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">
     <App />
-  </StrictMode>,
-)
+  </GoogleOAuthProvider>
+);
