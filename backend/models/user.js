53432caa02982a930adabbda13cf0499cb367c4d
Aadya2335 <aadya2335@gmail.com>
2025-10-11 09:47:10 +0530

    Add files via upload

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,1 +1,23 @@
+import mongoose from "mongoose";
 
+const UserSchema= new mongoose.Schema({
+    email : {type: String,unique:true, required: true,lowercase: true, trim:true},
+    passwordHash : { 
+        type:String,
+        required : function () {
+            return!this.googleID;
+        },
+        unique:false
+    },
+
+    googleID: {
+        type: String,
+        unique : function(){
+            return !!this.googleID;
+        },
+        sparse:true
+    }
+
+});
+
+export default mongoose.model("User",UserSchema);
